4c6582d5d7f3b58e2ff564b0a9e6e8b0cc798c01
Mason Herbel <me@artit.us>
2018-05-06 08:48:43 -0500

    Changed from GET requests to reading the path.

--- index.php
+++ index.php
@@ -3,25 +3,27 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Artitus' <?php echo $_GET["id"] . $_GET["type"]; ?></title>
+    <title>Artitus' File Uploader</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="https://s3.amazonaws.com/artit.us/branding/favicon.png">
     <link rel="apple-touch-icon" href="https://s3.amazonaws.com/artit.us/branding/favicon.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.css">\
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.css">
 
     <!-- Discord and other site embed support -->
+    <meta name="twitter:card" content="summary_large_image"/>
     <meta name="og:site_name" content="Ma.ssive.wang File Uploader"/>
     <meta property="og:type" content="website">
     <meta property="og:title" content="Artitus' File Uploader"/>
     <meta property="og:description"
-          content="Uploaded on <?php echo date("F d Y H:i:s.", filemtime($_GET["id"] . '.' . $_GET["type"])); ?>"/>
+          content="Uploaded on <?php $path = explode("/", $_SERVER['REQUEST_URI']); echo date("F d Y H:i:s.", filemtime('i/' . $path[2] . '.' . $path[1])); ?>"/>
     <meta property="og:url" content="<?php echo 'https:/' . $_SERVER['REQUEST_URI']; ?>"/>
     <?php
-    if ($_GET["type"] == "png" || $_GET["type"] == "gif" || $_GET["type"] == "jpg") {
-        list($width, $height) = getimagesize($_GET["id"] . '.' . $_GET["type"]);
-        echo '<meta property="og:image" content="https://' . $_SERVER['HTTP_HOST'] . '/' . $_GET["id"] . '.' . $_GET["type"] . '"/>';
-    } else if ($_GET["type"] == "mp4") {
-        echo '<meta property="og:video" content="https://' . $_SERVER['HTTP_HOST'] . '/' . $_GET["id"] . '.' . $_GET["type"] . '" />';
+    $path = explode("/", $_SERVER['REQUEST_URI']);
+    if ($path[1] == "png" || $path[1] == "gif" || $path[1] == "jpg") {
+        list($width, $height) = getimagesize($path[2] . '.' . $path[1]);
+        echo '<meta property="og:image" content="https://' . $_SERVER['HTTP_HOST'] . '/i/' . $path[2] . '.' . $path[1] . '"/>';
+    } else if ($path[1] == "mp4") {
+        echo '<meta property="og:video" content="https://' . $_SERVER['HTTP_HOST'] . '/i/' . $path[2] . '.' . $path[1] . '" />';
     }
     ?>
 </head>
@@ -40,7 +42,7 @@
         height: 100%;
         width: 100%;
         z-index: -1;
-        background: url(<?php echo 'https://' . $_SERVER['HTTP_HOST'] . '/' . $_GET["id"] . '.' . $_GET["type"]. ' '; ?>) no-repeat center center;
+        background: url(<?php $path = explode("/", $_SERVER['REQUEST_URI']); echo 'https://' . $_SERVER['HTTP_HOST'] . "/i/" . $path[2] . "." . $path[1] . ''; ?>) no-repeat center center;
         background-size: cover;
         filter: blur(5px);
         transform: scale(1.3)
@@ -77,11 +79,13 @@
 <div id="page">
     <div class="content">
         <?php
-        if ($_GET["type"] == "png" || $_GET["type"] == "gif" || $_GET["type"] == "jpg") {
-            echo '<img src="https://' . $_SERVER['HTTP_HOST'] . '/' . $_GET["id"] . '.' . $_GET["type"] . '"/>';
+        $path = explode("/", $_SERVER['REQUEST_URI']);
+        if ($path[1] == "png" || $path[1] == "gif" || $path[1] == "jpg") {
+            echo '<img src="https://' . $_SERVER['HTTP_HOST'] . '/i/' . $path[2] . '.' . $path[1] . '"/>';
         } else {
             echo 'y u not has gud lenk?';
-        } ?>
+        }
+        ?>
     </div>
 </div>
 
